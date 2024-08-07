8b6c4077845456c3db436a5aa47933cacb565a56
julioceaseless <julioceaseless@gmail.com>
2024-08-07 15:40:34 +0300

    decode basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python3
 """Basic authentication"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Basic auth"""
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        """extract authorization header"""
+
+        # check if authorization_header is valid
         if authorization_header is None or\
            not isinstance(authorization_header, str) or\
            not authorization_header.startswith('Basic '):
             return None
-        stripped_auth_header = authorization_header.lstrip('Basic').strip()
-        return stripped_auth_header
+
+        # strip the header to extract base64 part
+        base64_part = authorization_header.lstrip('Basic').strip()
+        return base64_part
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """decode base64 string"""
+        if base64_authorization_header is None or\
+           not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            # decode base64
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
