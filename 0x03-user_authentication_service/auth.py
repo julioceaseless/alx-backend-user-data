21f93dfe374402e120ba2c345c9f86722ce831b7
julioceaseless <julioceaseless@gmail.com>
2024-08-18 19:25:17 +0300

    fix bug

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,8 +3,8 @@
 import bcrypt
 
 
-def _hash_password(self, password: str) -> bytes:
-        """encrypts password"""
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
-        return hashed_password
+def _hash_password(password: str) -> bytes:
+    """encrypts password"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed_password
