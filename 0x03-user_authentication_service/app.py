d52ffb360f18f2f0e55c9188ddf4865e9089c019
julioceaseless <julioceaseless@gmail.com>
2024-08-19 19:01:19 +0300

    implement login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Simple flask app"""
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 
@@ -29,6 +29,24 @@ def users():
     except ValueError:
         return make_response(jsonify({"message": "email already registered"}),
                              400)
+
+
+@app.route("/sessions", methods=['POST'])
+def login():
+    """login user"""
+    # get user data from login form
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # check if credentials are valid
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    # create new session for user
+    session_id = AUTH.create_session(email)
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    # set cookies
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
