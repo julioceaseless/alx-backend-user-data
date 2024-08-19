c70164cb0d96b42af1ff9c0c887544430b354068
julioceaseless <julioceaseless@gmail.com>
2024-08-19 13:48:46 +0300

    modify uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,6 +13,11 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+def _generate_uuid(self) -> str:
+    """generate UUID"""
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -39,7 +44,3 @@ class Auth:
             return bcrypt.checkpw(password.encode('utf-8'),
                                   user.hashed_password)
         return False
-
-    def _generate_uuid(self) -> str:
-        """generate UUID"""
-        return str(uuid.uuid4())
