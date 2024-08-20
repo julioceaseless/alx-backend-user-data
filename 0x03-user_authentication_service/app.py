6140d6514c28f72183d0d80685df8684a32bd428
julioceaseless <julioceaseless@gmail.com>
2024-08-20 15:02:05 +0300

    set bad cookie

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -70,9 +70,10 @@ def profile():
     # get session_id from cookies
     session_id = request.cookies.get("session_id")
     # get user by session id
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        return jsonify({"email": user.email}), 200
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email}), 200
     abort(403)
 
 
