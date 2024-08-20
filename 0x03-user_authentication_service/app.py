a7723709fdb95a1cd889ddc0fe03c080d51b41c5
julioceaseless <julioceaseless@gmail.com>
2024-08-20 16:51:04 +0300

    fix bug

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -90,7 +90,7 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route("reset_password", methods=["PUT"])
+@app.route("/reset_password", methods=["PUT"])
 def update_password():
     """update password form"""
     email = request.form.get("email")
